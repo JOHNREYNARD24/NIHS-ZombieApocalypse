5c9bd2e7ad9f969659965771c1e0e9a58b07987a
JOHNREYNARD24 <loyloy112425@gmail.com>
2026-09-16 04:27:39 +0800

    Initialize game structure for NIHS Zombie Apocalypse

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+// NIHS Zombie Apocalypse
+// Main Game File
 
+const game = {
+    title: "NIHS Zombie Apocalypse",
+    language: "ceb",
+    currentLevel: 1,
+    currentRoom: 1,
+    running: false
+};
+
+console.log("NIHS Zombie Apocalypse - Game Foundation Loaded");
